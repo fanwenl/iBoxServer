7e1f1973fdd73daf0acf89fab53c70ebd3c43ae4
fanwenl <fanwenl_g@126.com>
2018-08-01 23:30:33 +0800

    update

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class publish_rtc(models.Model):
+    """ 统一下发RTC时间 """
+
+class publish_dac(models.Model):
+    """ 单独下发DAC的值 """
+
+class display_msg(models.Model):
+    """ 显示上报的MSG """
