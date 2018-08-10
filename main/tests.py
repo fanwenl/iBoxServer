384c6fc96d004f9b97ac500123a80e13e26d5f0a
= <=>
2018-08-10 14:45:31 +0800

    add test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
+from . import views
 
 # Create your tests here.
+
+class viewstest(TestCase):
+
+    def test_index(self):
+        response = self.client.get(reverse('main:index'))
+        self.assertEqual(response.status_code, 200)
